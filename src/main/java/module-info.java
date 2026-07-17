47a10897ee0dd5e21142d81a24d5a5daa84a25fd
edwindows11 <edwinbundan39@gmail.com>
2026-07-17 12:00:00 +0100

    build: fix Java source/target to 21, add surefire plugin for JUnit 5

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -4,4 +4,11 @@ module com.example.demo {
 
     opens com.example.demo to javafx.fxml;
     exports com.example.demo;
+    exports com.example.demo.core;
+    exports com.example.demo.model;
+    exports com.example.demo.ui;
+    exports com.example.demo.controller;
+    exports com.example.demo.util;
+    opens com.example.demo.core to javafx.fxml;
+    opens com.example.demo.ui to javafx.fxml;
 }
